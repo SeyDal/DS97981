f3ad4b391cd18343eeb646c6f9b0b726cfa0ab4b
seyyedmehdi seyyedalhosseini <mehdi6583@gmail.com>
2018-09-27 00:42:45 +0330

    A1 is done

---
+++ A1/A1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace A1
+{
+    public class Program
+    {
+        public static string Process(string input)
+        {
+            StringBuilder sb = new StringBuilder();
+            using (StringReader reader = new StringReader(input))
+            using (StringWriter writer = new StringWriter(sb))
+            {
+                int a = int.Parse(reader.ReadLine());
+                int b = int.Parse(reader.ReadLine());
+                writer.WriteLine(Add(a, b));
+            }
+            return sb.ToString();
+        }
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
